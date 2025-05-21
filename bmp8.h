5b9f57a3f5cf86a38a89a3bf4e7cf1032a89a15d
Sarra-Ghedira2005 <sarra.ghedira@efrei.net>
2025-05-21 19:29:25 +0200

    mettre les types et les constantes

--- bmp8.h
+++ bmp8.h
@@ -4,6 +4,21 @@
 
 #ifndef BMP8_H
 #define BMP8_H
+// Constantes pour les offsets des champs de l'en-tête BMP
+#define BITMAP_MAGIC 0x00 // offset 0
+#define BITMAP_SIZE 0x02 // offset 2
+#define BITMAP_OFFSET 0x0A // offset 10
+#define BITMAP_WIDTH 0x12 // offset 18
+#define BITMAP_HEIGHT 0x16 // offset 22
+#define BITMAP_DEPTH 0x1C // offset 28
+#define BITMAP_SIZE_RAW 0x22 // offset 34
+// Constante pour le type de fichier BMP
+#define BMP_TYPE 0x4D42 // 'BM' en hexadécimal
+// Constantes pour les tailles des champs de l'en-tête BMP
+#define HEADER_SIZE 0x0E // 14 octets
+#define INFO_SIZE 0x28 // 40 octets
+// Constantes pour les valeurs de profondeur de couleur
+#define DEFAULT_DEPTH 0x18 // 24
 
 
 typedef struct {
@@ -16,6 +31,34 @@ typedef struct {
     unsigned int dataSize; //représente la taille des données de l’image en octets (Située à l’offset 34 de l’en-tête).
 } t_bmp8;
 
+typedef struct {
+	uint16_t type;
+	uint32_t size;
+	uint16_t reserved1;
+	uint16_t reserved2;
+	uint32_t offset;
+} t_bmp_header;
+
+typedef struct {
+	uint32_t size;
+	int32_t width;
+	int32_t height;
+	uint16_t planes;
+	uint16_t bits;
+	uint32_t compression;
+	uint32_t imagesize;
+	int32_t xresolution;
+	int32_t yresolution;
+	uint32_t ncolors;
+	uint32_t importantcolors;
+} t_bmp_info;
+
+typedef struct {
+	uint8_t red;
+	uint8_t green;
+	uint8_t blue;
+} t_pixel;
+
 t_bmp8 *bmp8_loadImage(const char *filename);
 void bmp8_saveImage(const char *filename, t_bmp8 *img);
 void bmp8_printInfo(t_bmp8 *img);
@@ -24,5 +67,7 @@ void bmp8_negative(t_bmp8 * img);
 void bmp8_brightness(t_bmp8 * img, int value);
 void bmp8_threshold(t_bmp8 * img, int threshold);
 void bmp8_applyFilter(t_bmp8 *img, float **kernel, int kernelSize);
+
+
 
 #endif //BMP8_H
